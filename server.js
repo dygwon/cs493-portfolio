68307a163976161496d4be5adb5585af25e95d80
gwond <gwond@oregonstate.edu>
2021-06-01 22:09:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,39 @@
 // server.js
 
 
+/**
+ * Required External Modules
+ */
+
+
+/**
+ * App Variables
+ */
+
+
+/**
+ * Session Configuration
+ */
+
+
+/**
+ * Passport Configuration
+ */
+
+
+/**
+ * App Configuration
+ */
+
+
+/**
+ * Custom Middleware
+ */
+
+
+/**
+ * Router Mounting
+ */
+
+
+// Listen on App Engine-specified port of 8080
