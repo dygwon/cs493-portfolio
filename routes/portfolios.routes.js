556966ce042074e1651e3318ce7d600e9d1a0bcb
gwond <gwond@oregonstate.edu>
2021-06-02 22:10:54 -0400

    Create and test basic portfolio route connections

--- routes/portfolios.routes.js
+++ routes/portfolios.routes.js
@@ -1,12 +1,35 @@
 // ../routes/portfolios.routes.js
 
 
+/**
+ * TODO
+ * authenticate and create protected and unprotected routes for list portfolios
+ */
+
+
 const router = require('express').Router();
 
 
 router.route('/')
-    .get((err, req, res, next) => {
-        res.send("get portfolios");
+    .post((req, res) => {
+        res.status(200).send("creating a portfolio").end();
+    })
+    .get((req, res) => {
+        res.status(200).send("list portfolios").end();
+    });
+
+router.route('/:portfolioId')
+    .get((req, res) => {
+        res.status(200).send("getting specified portfolio").end();
+    })
+    .put((req, res) => {
+        res.status(200).send("update a portfolio (put)").end();
+    })
+    .patch((req, res) => {
+        res.status(200).send("update a portfolio (patch)").end();
+    })
+    .delete((req, res) => {
+        res.status(200).send("delete a portfolio").end();
     });
 
 
