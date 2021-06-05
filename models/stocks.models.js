8acfa312bdf396acb2535e713ad6823dd7f4e23f
gwond <gwond@oregonstate.edu>
2021-06-05 13:05:28 -0400

    Create model

--- models/stocks.models.js
+++ models/stocks.models.js
@@ -1,1 +1,27 @@
 // ../models/stocks.models.js
+
+
+class Stock {
+    constructor(ticker, company, portfolios=[]) {
+        this.portfolios = portfolios;
+        this.ticker = ticker;
+        this.company = company;
+    }
+
+    static fromReqBody(reqBody) {
+        return new Stock(
+            reqBody.ticker,
+            reqBody.company
+        );
+    }
+
+    static fromDatastore(data) {
+        return new Stock(
+            data.ticker,
+            data.company,
+            data.portfolios
+        );
+    }
+}
+
+module.exports = Stock;
