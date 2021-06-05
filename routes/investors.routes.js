e812e2cce9d035ea77129a5bde574a91ae8f600d
gwond <gwond@oregonstate.edu>
2021-06-05 09:57:39 -0400

    Add error checking

--- routes/investors.routes.js
+++ routes/investors.routes.js
@@ -2,13 +2,41 @@
 
 
 const router = require('express-promise-router')();
-const { check, validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
 
 router.route('/')
-    .post((req, res) => {
-        res.status(200).send("creating an investor").end();
-    })
+    .post(
+        body('firstName').isAlpha("en-US", { ignore: " -" }),
+        body('lastName').isAlpha("en-US", { ignore: " -" }),
+        (req, res) => {
+            
+            // check for valid request content type
+            if (!req.is('application/json')) {
+                return res.status(415).json({
+                    Error: "Requested with an unsupported MIME type"
+                });
+            }
+
+            // check for valid requested content type
+            const requestAccepts = req.get('accept');
+            if (requestAccepts !== 'application/json') {
+                return res.status(406).json({
+                    Error: "Requested an unsupported MIME type"
+                });
+            }
+
+            // send error if required parameter(s) missing
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    Error: "The request object is missing at least one of the required attributes"
+                });
+            }
+
+
+            res.status(200).send("creating an investor").end();
+        })
     .get((req, res) => {
         res.status(200).send("list investors").end();
     });
