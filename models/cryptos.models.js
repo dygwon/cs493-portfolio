f2a4eee4a7c73e919c64148d2138ced8d37d3233
gwond <gwond@oregonstate.edu>
2021-06-05 13:38:50 -0400

    Create model

--- models/cryptos.models.js
+++ models/cryptos.models.js
@@ -1,1 +1,27 @@
 // ../models/crypto.models.js
+
+
+class Crypto {
+    constructor(ticker, name, portfolios=[]) {
+        this.portfolios = portfolios;
+        this.ticker = ticker;
+        this.name = name;
+    }
+
+    static fromReqBody(reqBody) {
+        return new Crypto(
+            reqBody.ticker,
+            reqBody.name
+        );
+    }
+
+    static fromDatastore(data) {
+        return new Crypto(
+            data.ticker,
+            data.name,
+            data.portfolios
+        );
+    }
+}
+
+module.exports = Crypto;
