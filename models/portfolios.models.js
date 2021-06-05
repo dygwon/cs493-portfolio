5e8a9bd48398e28450c6b2f9964abb460f5c72d4
gwond <gwond@oregonstate.edu>
2021-06-05 11:32:41 -0400

    Create models

--- models/portfolios.models.js
+++ models/portfolios.models.js
@@ -1,1 +1,28 @@
 // ../models/portfolios.models.js
+
+
+class Portfolio {
+    constructor(owner=null, stocks=[], cryptos=[]) {
+        this.owner = owner;
+        this.stocks = stocks;
+        this.cryptos = cryptos;
+    }
+
+    static fromReqBody(sub, reqBody) {
+        return new Portfolio(
+            sub,
+            reqBody.stocks,
+            reqBody.cryptos
+        );
+    }
+
+    static fromDatastore(data) {
+        return new Portfolio(
+            data.owner,
+            data.stocks,
+            data.cryptos
+        );
+    }
+}
+
+module.exports = Portfolio;
