5f301612e8c1111132cfeca58461fcd981a130df
gwond <gwond@oregonstate.edu>
2021-06-06 16:27:41 -0400

    Configure express-session and passport

--- server.js
+++ server.js
@@ -10,17 +10,26 @@
  * - update investor should be protected?
  * - properties modeling relationships are not considered valid, add more properties
  * - what do you do with a portfolio that has no owner?
+ * - implement 405 status code
  */
 
 
 /**
  * Required External Modules
  */
+let dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 
+const session = require('express-session');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+
 // routers and middleware
+const userInViews = require('./lib/middleware/userInViews');
 const investorsRouter = require('./routes/investors.routes');
 const portfoliosRouter = require('./routes/portfolios.routes');
 const stocksRouter = require('./routes/stocks.routes');
@@ -37,11 +46,36 @@ const PORT = process.env.PORT || 8080;
 /**
  * Session Configuration
  */
+let sess = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUnitialized: true
+};
+
+if (app.get('env') === 'production') {
+    // use secure cookies in production
+    sess.cookie.secure = true;
+    sess.proxy = true;
+    app.set('trust proxy', 1);
+}
 
 
 /**
  * Passport Configuration
  */
+let strategy = new Auth0Strategy({
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:8080/callback'
+    },
+    function (accessToken, refreshToken, extraParams, profile, done) {
+        // extraParams.id_token carries the JSON Web Token
+        profile.id_token = extraParams.id_token;
+        return done(null, profile);
+    }
+);
 
 
 /**
@@ -59,10 +93,10 @@ app.use(logger('dev'));
 /**
  * Router Mounting
  */
- app.use('/investors', investorsRouter);
- app.use('/portfolios', portfoliosRouter);
- app.use('/stocks', stocksRouter);
- app.use('/cryptos', cryptoRouter);
+app.use('/investors', investorsRouter);
+app.use('/portfolios', portfoliosRouter);
+app.use('/stocks', stocksRouter);
+app.use('/cryptos', cryptoRouter);
 
 
 // GET Homepage
